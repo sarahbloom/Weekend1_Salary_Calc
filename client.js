e83eb6d95ac0e01652aa20bbdf0bbf8b65e62efd
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-09 16:09:50 -0600

    posting inputs to dom

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6*/
+
 console.log('js');
 
 $(document).ready(readyNow); {
@@ -5,4 +7,18 @@ $(document).ready(readyNow); {
 
 function readyNow() {
   console.log("JQ");
+  $('#submit').on('click', addEmployee);
+}
+
+function addEmployee(){
+  console.log('adding employee');
+  let fName = $('#firstName').val();
+  let lName = $('#lastName').val();
+  let id = $('#idNumber').val();
+  let title = $('#jobTitle').val();
+  let annualSalary = $('#salary').val();
+  let tableRow = ('<tr><td>' + fName + '</td><td>' + lName + '</td><td>' + id + '</td><td>' +title + '</td)<td>' + annualSalary + '</td></tr>');
+  console.log(tableRow);
+
+  $('#employeeTable').append(tableRow);
 }
