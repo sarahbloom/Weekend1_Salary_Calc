53c55f463ffe8a2b669ac652eda65d14bc566197
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-11 13:30:15 -0500

    updated delete line

--- client.js
+++ client.js
@@ -14,17 +14,12 @@ class Employee {
   }
 }
 
-const employee1 = new Employee('Jessica', 'Jones', '234', "detective", '48000');
-const employee2 = new Employee('Mary', 'Poppins', '1882', 'nanny', '36000');
-const employee3 = new Employee('Charlie', 'Brown', '2346', 'teacher', '36000');
-const employee4 = new Employee('Jack', 'Sprat', '980', 'cook', '360000');
-
 $(document).ready(readyNow); {}
 
 function readyNow() {
   console.log("JQ");
   $('#submit').on('click', addEmployee);
-  $('#delete').on('click', removeEmployee);
+  $('#employeeTable').on('click', '#delete', removeEmployee);
 }
 
 function addEmployee() {
@@ -37,11 +32,11 @@ function addEmployee() {
   salaries.push(parseInt(annualSalary));
   //console.log(salaries);
   let tableRow = ('<tr><td>' + fName + '</td><td>' + lName + '</td><td>' +
-    id + '</td><td>' + title + '</td><td>' + annualSalary + '</td></tr>');
+                id + '</td><td>' + title + '</td><td>' + annualSalary +
+                '</td><td>' + '<button id="delete">Delete</button>' + '</td></tr>');
   $('#employeeTable').append(tableRow);
   $('.input').val('');
   totalSalaries(salaries);
-  console.log(fName);
   addToArray(fName, lName, id, title, annualSalary);
 }
 
@@ -53,8 +48,9 @@ function addToArray(fName, lName, id, title, annualSalary) {
 
 function removeEmployee() {
   console.log('in remove employee');
-  $("#table tr:last").remove();
+  $(this).parent().parent().remove();
 }
+
 function totalSalaries(salaryArray) {
   let payment = 0;
   for (let i = 0; i < salaryArray.length; i++) {
